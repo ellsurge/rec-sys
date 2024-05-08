9ebe22af41abfd47ac0cd6a997f05b870fbfe581
ellsurge <iorsasesesugh@gmail.com>
2024-05-08 04:30:06 +0100

    fixed mobile nav

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -89,25 +89,15 @@ export const Navbar = () => {
 
 			<NavbarMenu>
 				{searchInput}
-				<div className="mx-4 mt-2 flex flex-col gap-2">
-					{siteConfig.navMenuItems.map((item, index) => (
-						<NavbarMenuItem key={`${item}-${index}`}>
-							<Link
-								color={
-									index === 2
-										? "primary"
-										: index === siteConfig.navMenuItems.length - 1
-										? "danger"
-										: "foreground"
-								}
-								href="#"
-								size="lg"
-							>
-								{item.label}
-							</Link>
-						</NavbarMenuItem>
-					))}
-				</div>
+					<Button
+						as={NextLink}
+						className="text-sm font-normal text-default-600 bg-default-100"
+						href="/add"
+						startContent={<HeartFilledIcon className="text-primary" />}
+						variant="flat"
+					>
+						Add Item
+					</Button>
 			</NavbarMenu>
 		</NextUINavbar>
 	);
